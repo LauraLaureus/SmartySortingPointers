0dcdf3b98ba32524375641af31b662f522a9cdff
Laura <l4depende@gmail.com>
2016-11-25 20:33:40 +0100

    Initial Commit

---
+++ SmartyPointers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SmartyPointers
+//
+//  Created by Laura del Pino Díaz on 25/11/16.
+//  Copyright © 2016 Laura del Pino Díaz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
